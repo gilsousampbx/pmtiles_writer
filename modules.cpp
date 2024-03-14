f8383c52edea2bde2d9e0a45493ec042c38bc6ff
Gil Sousa <gil.sousa@mapbox.com>
2024-03-14 18:31:21 +0000

    Receive array of properties and return a response object

--- modules.cpp
+++ modules.cpp
@@ -2,6 +2,7 @@
 #include "lib/pmtiles.hpp"
 #include "lib/nlohmann/json.hpp"
 #include "lib/writer.cpp"
+#include <iostream>
 
 using v8::FunctionCallbackInfo;
 using v8::Isolate;
@@ -11,9 +12,52 @@ using v8::String;
 using v8::Value;
 
 void GeneratePMTilesBundle(const FunctionCallbackInfo<Value>& args) {
-    Isolate* isolate = args.GetIsolate();
+    v8::Isolate* isolate = args.GetIsolate();
+
+    if (!args[0]->IsArray()) {
+        isolate->ThrowException(v8::Exception::TypeError(
+            v8::String::NewFromUtf8(isolate, "Argument must be an array").ToLocalChecked()));
+        return;
+    }
+
+    v8::Local<v8::Array> tiles_arr = v8::Local<v8::Array>::Cast(args[0]);
 
     Writer writer;
+
+    for (size_t i = 0; i < tiles_arr->Length(); ++i) {
+        Local<Value> t_item = tiles_arr->Get(isolate->GetCurrentContext(), i).ToLocalChecked();
+
+        if (!t_item->IsObject()) {
+            isolate->ThrowException(v8::Exception::TypeError(
+                String::NewFromUtf8(isolate, "Items in the tiles array must be objects with the fields 'z', 'x', 'y', and 'buffer'").ToLocalChecked()));
+            return;
+        }
+
+        Local<Object> t_obj = t_item->ToObject(isolate->GetCurrentContext()).ToLocalChecked();
+
+        int z = t_obj->Get(
+            isolate->GetCurrentContext(), 
+            String::NewFromUtf8(isolate, "z").ToLocalChecked()
+        ).ToLocalChecked()->Int32Value(isolate->GetCurrentContext()).ToChecked();
+
+        int x = t_obj->Get(
+            isolate->GetCurrentContext(), 
+            String::NewFromUtf8(isolate, "x").ToLocalChecked()
+        ).ToLocalChecked()->Int32Value(isolate->GetCurrentContext()).ToChecked();
+
+        int y = t_obj->Get(
+            isolate->GetCurrentContext(), 
+            String::NewFromUtf8(isolate, "y").ToLocalChecked()
+        ).ToLocalChecked()->Int32Value(isolate->GetCurrentContext()).ToChecked();
+
+        std::string tile = *String::Utf8Value(
+            isolate, 
+            t_obj->Get(isolate->GetCurrentContext(), String::NewFromUtf8(isolate, "buffer").ToLocalChecked()).ToLocalChecked()
+        );
+
+        writer.write_tile(z, x, y, tile);
+        std::cout << tile << std::endl;
+    }
 
     pmtiles::headerv3 header;
     header.tile_type = pmtiles::TILETYPE_UNKNOWN;
@@ -32,13 +76,26 @@ void GeneratePMTilesBundle(const FunctionCallbackInfo<Value>& args) {
         {"attribution", "Map tiles by <a href='http://stamen.com'>Stamen Design</a>, under <a href='http://creativecommons.org/licenses/by/3.0'>CC BY 3.0</a>. Data by <a href='http://openstreetmap.org'>OpenStreetMap</a>, under <a href='http://www.openstreetmap.org/copyright'>ODbL</a>."},
     };
 
-    writer.write_tile(0, 0, 0, "my tile 1");
-    writer.write_tile(1, 0, 0, "my tile 2");
-    writer.write_tile(2, 0, 0, "my tile 3");
+    PMTilesBundlerResponse response = writer.finalize(header, metadata);
 
-    writer.finalize(header, metadata);
+    Local<String> bufferString = String::NewFromUtf8(
+        isolate,
+        response.buffer.str().c_str(),
+        v8::NewStringType::kNormal,
+        response.buffer.str().length()
+    ).ToLocalChecked();
 
-    args.GetReturnValue().Set(String::NewFromUtf8(isolate, metadata.dump().data()).ToLocalChecked());
+    Local<Object> responseObj = v8::Object::New(isolate);
+
+    responseObj->Set(isolate->GetCurrentContext(), String::NewFromUtf8(isolate, "leaf_size", v8::NewStringType::kNormal).ToLocalChecked(), v8::Integer::New(isolate, response.leaf_size)).FromJust();
+
+    responseObj->Set(
+        isolate->GetCurrentContext(),
+        String::NewFromUtf8(isolate, "buffer").ToLocalChecked(),
+        bufferString
+    );
+    
+    args.GetReturnValue().Set(responseObj);
 }
 
 void Initialize(Local<Object> exports) {
