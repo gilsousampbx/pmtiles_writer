04ebf6932a6857c22d0f70d166cb88767ae44faa
Gil Sousa <gil.sousa@mapbox.com>
2024-03-14 18:31:49 +0000

    update test file

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 const addon = require('./build/Release/modules');
 
-addon.generate_pmtiles_bundle();
+const z = 0;
+const x = 0;
+const y = 0;
+const buffer = 'my tile';
+
+const response = addon.generate_pmtiles_bundle([{z, x, y, buffer}, {z, x, y, buffer}]);
+console.log(response)
