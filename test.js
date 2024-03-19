2aad52c1c06a7bdc7e8e0d9b04b822448341fc5d
Attila Kiss <attila.kiss@mapbox.com>
2024-03-19 15:58:28 +0200

    Real test data

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ var path = require('path');
 var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
 console.log(binding_path);
 var binding = require(binding_path);
+var fs = require('fs');
 
 
 
@@ -11,14 +12,48 @@ const x = 0;
 const y = 0;
 const buffer = 'my tile';
 const vector_layers = [
-  {"id": "layer1", "fields": {"hello": "string", "population": "number", "hola": "boolean"}, "minzoom": 0, "maxzoom": 3},
-  {"id": "layer2", "fields": {"hoi": "string"}, "minzoom": 5, "maxzoom": 5},
+  {"id": "layer1", "fields": {"hello": "string", "population": "number", "hola": "boolean"}, "minzoom": 0, "maxzoom": 1},
+  {"id": "layer2", "fields": {"hoi": "string"}, "minzoom": 0, "maxzoom": 1},
 ];
 const metadata = {
   "name": "user.tileset_name",
   "vector_layers": vector_layers,
-  "sharding_scheme_version": "v1",
+  "sharding_scheme_version": "single_shard",
 };
 
-const response = binding.generate_pmtiles_bundle([{ z, x, y, buffer }, { z, x, y, buffer }], JSON.stringify(metadata));
+const tiles = [
+  {
+    buffer: fs.readFileSync(`${__dirname}/0-0-0.mvt`),
+    z: 0,
+    x: 0,
+    y: 0,
+  },
+  {
+    buffer: fs.readFileSync(`${__dirname}/1-0-0.mvt`),
+    z: 1,
+    x: 0,
+    y: 0,
+  },
+  {
+    buffer: fs.readFileSync(`${__dirname}/1-1-0.mvt`),
+    z: 1,
+    x: 1,
+    y: 0,
+  },
+  {
+    buffer: fs.readFileSync(`${__dirname}/1-0-1.mvt`),
+    z: 1,
+    x: 0,
+    y: 1,
+  },
+  {
+    buffer: fs.readFileSync(`${__dirname}/1-1-1.mvt`),
+    z: 1,
+    x: 1,
+    y: 1,
+  },
+];
+
+const response = binding.generate_pmtiles_bundle(tiles, JSON.stringify(metadata));
+fs.writeFileSync(`${__dirname}/bundle.pmtiles`, response.buffer);
 console.log(response);
