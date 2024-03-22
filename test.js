1562fc4d34e9e37501632527389d8143b89c9c7c
Gil Sousa <gil.sousa@mapbox.com>
2024-03-22 07:35:00 +0000

    Update test script

--- test.js
+++ test.js
@@ -1,10 +1,7 @@
 var binary = require('@mapbox/node-pre-gyp');
 var path = require('path');
 var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
-console.log(binding_path);
-// var binding = require(binding_path);
-
-var binding = require("./build/Debug/modules.node");
+var binding = require(binding_path);
 var fs = require('fs');
 
 
